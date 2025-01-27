a61e766d73912c6b01c76512b83df64eb85e38ec
Sam Mitchinson <s.mitchinson@uea.ac.uk>
2025-01-27 17:02:58 +0000

    Update redirect.js
    This is just a stand-in function until I create the database

--- api/redirect.js
+++ api/redirect.js
@@ -3,13 +3,7 @@ module.exports = (req, res) => {
         "https://form.jotform.com/250264561457357",
         "https://form.jotform.com/smitchinson/uea"
     ];
-    // Retrieve the index from the query string or start at 0
-    const index = parseInt(req.query.index, 10) || 0;
-
-    // Calculate the next index, wrapping around if necessary
-    const nextIndex = (index + 1) % urls.length;
-
-    // Redirect to the current URL with the next index as a query parameter
-    res.writeHead(302, { Location: `${urls[index]}?index=${nextIndex}` });
+    let index = Math.floor(Math.random() * urls.length); // Random selection to demonstrate functionality
+    res.writeHead(302, { Location: urls[index] });
     res.end();
 };

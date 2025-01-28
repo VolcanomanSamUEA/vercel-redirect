0591b47124fbb28cc7321f58416599ccb4b50cb1
Sam Mitchinson <s.mitchinson@uea.ac.uk>
2025-01-28 12:40:15 +0000

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,9 @@
 module.exports = (req, res) => {
     const urls = [
-        "https://form.jotform.com/250264561457357",
-        "https://form.jotform.com/smitchinson/uea"
+        "https://form.jotform.com/smitchinson/cove-b1",
+        "https://form.jotform.com/smitchinson/cove-b2",
+        "https://form.jotform.com/smitchinson/cove-b3",
+        "https://form.jotform.com/smitchinson/cove-b4"
     ];
     let index = Math.floor(Math.random() * urls.length); // Random selection to demonstrate functionality
     res.writeHead(302, { Location: urls[index] });
